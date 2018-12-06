12ae7bca0cb4e1bce2dbde3bc28836f62ca11b9f
Jam Risser <jam@codejam.ninja>
2018-12-05 19:04:17 -0600

    Updated to match babel stages

--- src/index.js
+++ src/index.js
@@ -5,30 +5,27 @@ export default function() {
       '@babel/plugin-proposal-function-bind',
 
       // Stage 1
-      '@babel/plugin-proposal-export-default-from',
-      '@babel/plugin-proposal-logical-assignment-operators',
-      ['@babel/plugin-proposal-optional-chaining', { loose: false }],
-      ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
-      ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: false }],
       '@babel/plugin-proposal-do-expressions',
+      'babel-plugin-transform-export-extensions',
+      'babel-plugin-transform-numeric-separator',
+      ['@babel/plugin-proposal-optional-chaining', { loose: false }],
 
       // Stage 2
+      '@babel/plugin-proposal-function-sent',
+      '@babel/plugin-syntax-import-meta',
       [
         '@babel/plugin-proposal-decorators',
         {
           legacy: true
         }
       ],
-      '@babel/plugin-proposal-function-sent',
-      '@babel/plugin-proposal-export-namespace-from',
-      '@babel/plugin-proposal-numeric-separator',
-      '@babel/plugin-proposal-throw-expressions',
 
       // Stage 3
       '@babel/plugin-syntax-dynamic-import',
-      '@babel/plugin-syntax-import-meta',
-      ['@babel/plugin-proposal-class-properties', { loose: true }],
-      '@babel/plugin-proposal-json-strings'
+      '@babel/plugin-transform-modern-regexp',
+      'babel-plugin-transform-dotall-regex',
+      'babel-plugin-transform-unicode-property-regex',
+      ['@babel/plugin-proposal-class-properties', { loose: true }]
     ]
   };
 }
