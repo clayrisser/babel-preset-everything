7af7ef47e5094611d70031e87f2adcc6d3eddcc5
Jam Risser <jam@codejam.ninja>
2019-08-14 01:35:29 -0500

    v0.4.2

--- src/index.js
+++ src/index.js
@@ -1,48 +1,36 @@
-import path from 'path';
-import pkgDir from 'pkg-dir';
 import { declare } from '@babel/helper-plugin-utils';
 
-export default declare(() => {
+export default declare((_api, _opts) => {
   return {
     plugins: [
       // Stage 0
-      resolve('@babel/plugin-proposal-function-bind'),
+      '@babel/plugin-proposal-function-bind',
 
       // Stage 1
-      resolve('@babel/plugin-proposal-do-expressions'),
-      resolve('@babel/plugin-syntax-export-extensions'),
-      resolve('@babel/plugin-proposal-numeric-separator'),
-      [resolve('@babel/plugin-proposal-optional-chaining'), { loose: false }],
+      '@babel/plugin-proposal-do-expressions',
+      '@babel/plugin-syntax-export-extensions',
+      '@babel/plugin-proposal-numeric-separator',
+      ['@babel/plugin-proposal-optional-chaining', { loose: false }],
 
       // Stage 2
-      resolve('@babel/plugin-proposal-function-sent'),
-      resolve('@babel/plugin-syntax-import-meta'),
+      '@babel/plugin-proposal-function-sent',
+      '@babel/plugin-syntax-import-meta',
       [
-        resolve('@babel/plugin-proposal-decorators'),
+        '@babel/plugin-proposal-decorators',
         {
           legacy: true
         }
       ],
 
       // Stage 3
-      resolve('@babel/plugin-proposal-unicode-property-regex'),
-      resolve('@babel/plugin-syntax-dynamic-import'),
-      resolve('@babel/plugin-transform-dotall-regex'),
-      resolve('babel-plugin-transform-modern-regexp'),
-      [resolve('@babel/plugin-proposal-class-properties'), { loose: true }],
+      '@babel/plugin-proposal-unicode-property-regex',
+      '@babel/plugin-syntax-dynamic-import',
+      '@babel/plugin-transform-dotall-regex',
+      'babel-plugin-transform-modern-regexp',
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
 
       // Stage 4
-      resolve('@babel/plugin-transform-runtime')
+      '@babel/plugin-transform-runtime'
     ]
   };
 });
-
-function resolve(packageName, dirname = __dirname, paths = []) {
-  return require.resolve(packageName, {
-    paths: [
-      path.resolve(pkgDir.sync(process.cwd()), 'node_modules'),
-      path.resolve(pkgDir.sync(dirname), 'node_modules'),
-      ...paths
-    ]
-  });
-}
